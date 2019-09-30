006536864baebb89f3631a44224f333859375e0e
jaykay98 <38643191+jaykay98@users.noreply.github.com>
2019-10-01 01:35:49 +1000

    Update index.js

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -20,28 +20,6 @@ export default {
     }
   },
   actions: {
-    signUserUp({ commit }, payload) {
-      commit("setLoading", true);
-      commit("clearError");
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(user => {
-          commit("setLoading", false);
-          const newUser = {
-            id: user.uid,
-            name: user.displayName,
-            email: user.email,
-            photoUrl: user.photoURL
-          };
-          commit("setUser", newUser);
-        })
-        .catch(error => {
-          commit("setLoading", false);
-          commit("setError", error);
-          console.log(error);
-        });
-    },
     signUserIn({ commit }, payload) {
       commit("setLoading", true);
       commit("clearError");
